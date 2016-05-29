1c6773113b6380b13ecabf4b7be557f04ec236f4
MLauper <laupm4@bfh.ch>
2016-05-29 15:07:28 +0200

    Initial commit v2

--- Doors_and_Keys.pl
+++ Doors_and_Keys.pl
@@ -1,11 +1,15 @@
 treasure_in(room2).
+/* door(FROM, TO, KEY) */
 door(room0,room1,key1).
 door(room1,room2,key2).
-contains_keys(room0,[key1, key99]).
-contains_keys(room1,[key2]).
+contains_key(room0,key1).
+contains_key(room0,key99).
+contains_key(room1,key2).
 
 /* Player starts in a room with a list of keys */
+/* play(ROOM, PLAYER_KEYS, STEPS) :- pickup_key(ROOM,PLAYER_KEYS,PLAYER_KEYS_EXT), treasure(ROOM). */
 
+/* play(ROOM, PLAYER_KEYS) */
 play(ROOM, _, STEPS_IN, STEPS_OUT) :-
 	treasure_in(ROOM),
 	string_concat("Treasure found in Room ", ROOM, MESSAGE),
@@ -24,19 +28,10 @@ switchRoom(ROOM, PLAYER_KEYS, NEW_ROOM, STEPS_IN, STEPS_OUT) :-
 	string_concat(MESSAGE3, NEW_ROOM, MESSAGE4),
 	append(STEPS_IN, [MESSAGE4], STEPS_OUT).
 pickupKey(ROOM, PLAYER_KEYS, NEW_PLAYER_KEYS, STEPS_IN, STEPS_OUT) :-
-	contains_keys(ROOM,KEYS),
-	append(PLAYER_KEYS, KEYS, NEW_PLAYER_KEYS),
-	atomics_to_string(KEYS, ", ", MESSAGE1),
-	string_concat("Pickup Keys ", MESSAGE1, MESSAGE2),
-	string_concat(MESSAGE2, " in Room", MESSAGE3),
+	contains_key(ROOM,ROOM_KEY),
+	not(member(ROOM_KEY, PLAYER_KEYS)),
+	append(PLAYER_KEYS, [ROOM_KEY], NEW_PLAYER_KEYS),
+	atomic_concat("Pickup Key ", ROOM_KEY, MESSAGE2),
+	string_concat(MESSAGE2, " in Room ", MESSAGE3),
 	string_concat(MESSAGE3, ROOM, MESSAGE4),
 	append(STEPS_IN, [MESSAGE4], STEPS_OUT).
-
-
-
-
-
-
-
-
-
